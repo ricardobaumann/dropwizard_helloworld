d450e4567661832cb2c71695d3daf8071fd18ded
Ricardo Baumann <ricardobaumann@goeuro.com>
2016-09-08 20:35:12 +0200

    project scaffold

---
+++ src/main/java/com/github/ricardobaumann/dropwizard_helloworldConfiguration.java
@@ -0,0 +1,10 @@
+package com.github.ricardobaumann;
+
+import io.dropwizard.Configuration;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.hibernate.validator.constraints.*;
+import javax.validation.constraints.*;
+
+public class dropwizard_helloworldConfiguration extends Configuration {
+    // TODO: implement service configuration
+}
